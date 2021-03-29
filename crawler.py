c9ddbd37579b5be3804954b737f5e7d6dba1acaa
Dennis Lan <kueiyulan@gmail.com>
2021-03-30 01:12:55 +0800

    XOR hot fix

--- crawler.py
+++ crawler.py
@@ -11,7 +11,10 @@ from urllib.request import urlopen
 import os 
 import sys 
 import pandas as pd
+from datetime import date
 
+today = date.today()
+d = today.strftime("%Y-%b-%d")
 raw_request = Request('https://stock.wespai.com/rate110')
 raw_request.add_header('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:78.0) Gecko/20100101 Firefox/78.0')
 raw_request.add_header('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
@@ -49,25 +52,24 @@ for table in tables:
             list_of_cells.append(text)
         list_of_rows.append(list_of_cells)
     tableMatrix = list_of_rows
-# print(tableMatrix)
 # DataFrame  
 dataFrame = pd.DataFrame(data = tableMatrix, columns = list_header) 
-# print(dataFrame['代號'])
-# print(list_of_rows[0][0])
 
 tableMatrix = []
 for table in pasttables:
-    #Here you can do whatever you want with the data! You can findAll table row headers, etc...
     list_of_rows = []
     for row in table.findAll('tr')[1:]:
         list_of_cells = []
         for cell in row.findAll('td'):
             text = cell.text.replace('&nbsp;', '')
             list_of_cells.append(text)
-        if list_of_cells[0] not in dataFrame['代號']:
+        if list_of_cells[0] not in dataFrame['代號'].tolist():
             list_of_rows.append(list_of_cells)
     tableMatrix = list_of_rows
-# print(tableMatrix)
-# Converting Pandas DataFrame 
+    
+# Converting Pandas DataFrame
+# DataFrame  
+newDataFrame = pd.DataFrame(data = tableMatrix, columns = list_header)
+print(newDataFrame)
 # into CSV file 
-dataFrame.to_csv('~/Desktop/today.csv', encoding='utf_8_sig')
+newDataFrame.to_csv('~/Desktop/'+d+'.csv', encoding='utf_8_sig')
