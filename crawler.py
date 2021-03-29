82a476ef4cedf575ecb409f5fcc0699664d053fd
Dennis Lan <kueiyulan@gmail.com>
2021-03-30 00:37:33 +0800

    XOR two tables

--- crawler.py
+++ crawler.py
@@ -22,7 +22,16 @@ soup = BeautifulSoup(raw_html,"html.parser")
 tables = soup.findAll("table")
 list_header = [] 
 header = soup.find_all("table")[0].find("tr") 
-  
+
+past_request = Request('https://stock.wespai.com/rate109')
+past_request.add_header('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:78.0) Gecko/20100101 Firefox/78.0')
+past_request.add_header('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
+resppast = urlopen(past_request)
+past_html = resppast.read()
+souppast = BeautifulSoup(past_html,"html.parser")
+
+pasttables = souppast.findAll("table")
+
 for items in header: 
     try: 
         list_header.append(items.get_text()) 
@@ -43,8 +52,22 @@ for table in tables:
 # print(tableMatrix)
 # DataFrame  
 dataFrame = pd.DataFrame(data = tableMatrix, columns = list_header) 
-   
+# print(dataFrame['代號'])
+# print(list_of_rows[0][0])
+
+tableMatrix = []
+for table in pasttables:
+    #Here you can do whatever you want with the data! You can findAll table row headers, etc...
+    list_of_rows = []
+    for row in table.findAll('tr')[1:]:
+        list_of_cells = []
+        for cell in row.findAll('td'):
+            text = cell.text.replace('&nbsp;', '')
+            list_of_cells.append(text)
+        if list_of_cells[0] not in dataFrame['代號']:
+            list_of_rows.append(list_of_cells)
+    tableMatrix = list_of_rows
+# print(tableMatrix)
 # Converting Pandas DataFrame 
 # into CSV file 
-dataFrame.to_csv('today.csv', encoding='utf_8_sig')
-
+dataFrame.to_csv('~/Desktop/today.csv', encoding='utf_8_sig')
